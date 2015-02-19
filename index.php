86d124a86eab83b7de51fb836b0aeca46c02ec73
chivax <chiva.uribe@gmail.com>
2015-02-19 14:50:32 -0600

    Inicial

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8" />
+	<meta name="description" content="Directorio Comercial con rutas dinamicas desde donde estes." />
+	<title>Buscame , Bienvenido empieza a buscar.</title>
+	<link rel="stylesheet" href="stilo/normalize.css" />
+	<link rel="stylesheet" href="stilo/estilos.css" />
+	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<header>
+		<figure id="logo">
+					<img src="images/name.png" />
+		</figure>
+	</header>
+	<nav></nav>
+	<section>
+		<article></article>
+	</section>
+	<footer>
+		<p>
+			<strong>Powered by Hostiux y Softiux</strong>
+		</p>
+		<p>
+			MUC 2015
+		</p>
+	</footer>
+</body>
+
+</html>
